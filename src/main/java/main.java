b29b5560e0d331225c776bb03322e8f1d3ab609f
osavenko <osavenko@gmail.com>
2019-12-24 17:36:35 +0200

    Finished class AvalBank

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,4 @@
+import model.AvalBank;
 import model.CurrencyPair;
 import model.PrivateBank;
 
@@ -5,10 +6,18 @@ import java.util.List;
 
 public class main {
     public static void main(String[] args) {
+        System.out.println("Курсы ПриватБанка");
         PrivateBank pb = new PrivateBank();
         List<CurrencyPair> currencyPairsPB = pb.getCurrenciesPairs();
         for (CurrencyPair c:currencyPairsPB){
             System.out.println(c);
         }
+        System.out.println("Курсы Аваль");
+        AvalBank ab = new AvalBank();
+        List<CurrencyPair> currencyPairsAB = ab.getCurrenciesPairs();
+        for (CurrencyPair c:currencyPairsAB){
+            System.out.println(c);
+        }
+
     }
 }
--- src/main/java/model/AvalBank.java
+++ src/main/java/model/AvalBank.java
@@ -3,6 +3,7 @@ package model;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,6 +17,18 @@ public class AvalBank implements Strategy {
         try {
             Document document = getDocument();
             currencyPairList = new ArrayList<CurrencyPair>();
+            Elements bodyCurrencys = document.getElementsByClass("body-currency-block-rba");
+            if(!bodyCurrencys.isEmpty()){
+                Elements trs = bodyCurrencys.get(0).getElementsByTag("tr");
+                trs.remove(0);
+                for (Element tr: trs){
+                    Elements tds = tr.getElementsByTag("td");
+                    CurrencyPair currencyPair = getPair(tds);
+                    if (currencyPair!=null){
+                        currencyPairList.add(currencyPair);
+                    }
+                }
+            }
 
         }catch (IOException ignore){
             ignore.printStackTrace();
@@ -27,5 +40,30 @@ public class AvalBank implements Strategy {
                 .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
                 .get();
         return document;
+    }
+    private CurrencyPair getPair(Elements el){
+        CurrencyPair currencyPair = null;
+        Currency baseCurrency = new Currency("UAH");
+        Currency secondCurrency = null;
+        float buy = 0;
+        float sale = 0;
+        if (el.size()==3){
+
+            if(el.get(0).text().equals("Доллары США")){
+                secondCurrency = new Currency("USD");
+            } else if(el.get(0).text().equals("Евро")){
+                secondCurrency = new Currency("EUR");
+            } else if(el.get(0).text().equals("Рубли")){
+                secondCurrency = new Currency("RUR");
+            } else if(el.get(0).text().equals("Английские фунты стерлингов")){
+                secondCurrency = new Currency("GBP");
+            } else if(el.get(0).text().equals("Швейцарские франки")){
+                secondCurrency = new Currency("CHF");
+            }
+            buy = Float.parseFloat(el.get(1).text().replace(',','.'));
+            sale = Float.parseFloat(el.get(2).text().replace(',','.'));
+            currencyPair = new CurrencyPair(baseCurrency, secondCurrency, buy, sale);
+        }
+        return currencyPair;
     }
 }
