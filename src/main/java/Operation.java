faf4a3d5992a8b3ebe3077679a9821208e861c14
osavenko <osavenko@gmail.com>
2019-12-25 15:46:24 +0200

    Add enum Operation

---
+++ src/main/java/Operation.java
@@ -0,0 +1,18 @@
+import exxeption.InterruptOperationException;
+
+public enum Operation {
+    KURS_PRIVATEBANK,
+    KURS_AVALBANK,
+    KURS_OSCHADBANK;
+    public static Operation getOperationByOrdinal(int i) throws InterruptOperationException {
+        switch (i){
+            case 1:
+                return Operation.KURS_PRIVATEBANK;
+            case 2:
+                return Operation.KURS_OSCHADBANK;
+            case 3:
+                return Operation.KURS_AVALBANK;
+        }
+        throw new InterruptOperationException();
+    }
+}
